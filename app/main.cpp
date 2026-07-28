fc79067acb5db5591984ee906c3a5e7eaad24fcf
hPrevInstance <13481084642@163.com>
2026-07-28 22:30:47 +0800

    feat: 初始化项目结构，完成 Qt 环境交叉编译验证

--- app/main.cpp
+++ app/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QPushButton button("我的第一个 Windows 程序 (WSL编译)");
+    QPushButton button("我的第一个 Windows 程序");
     button.resize(300, 200);
     button.show();
     return app.exec();
